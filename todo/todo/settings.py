bc8290005cd3b31702e900fd19a7d7a9c3279347
alitoshenko <aleksandralitoshenko@gmail.com>
2022-09-05 21:16:14 +0300

    add webinar changes

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks.apps.TasksConfig'
+    'tasks.apps.TasksConfig',
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("tasks.urls")),
+    path("api/", include("api.urls")),
     path('admin/', admin.site.urls),
 ]
 
