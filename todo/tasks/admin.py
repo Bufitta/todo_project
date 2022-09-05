15752ddbcb72b3acfbfa11d32018383b17daf862
alitoshenko <aleksandralitoshenko@gmail.com>
2022-09-05 13:02:45 +0300

    add user model

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -3,7 +3,7 @@ from .models import Category, Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'category', 'priority', 'deadline', 'done', 'description')
+    list_display = ('pk', 'title', 'category', 'user', 'priority', 'deadline', 'done', 'description')
     search_fields = ('title',)
     list_filter = ('done', 'deadline', 'category', 'priority')
     empty_value_display = '------'
--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -25,6 +28,7 @@ class Task(models.Model):
     ]
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=255)
     deadline = models.DateTimeField(null=True, blank=True)
     description = models.TextField(blank=True, null=True)
