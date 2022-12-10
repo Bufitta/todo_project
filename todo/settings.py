6868abd3c6d21ad1d3ba76d4212b8a6f89f2bd83
alitoshenko <aleksandralitoshenko@gmail.com>
2022-12-11 00:42:47 +0300

    comment staticfiles dirs

--- todo/settings.py
+++ todo/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project_static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project_static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
