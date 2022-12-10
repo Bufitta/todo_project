26db2a16df43bc52f2bb24f728f944d80283594a
alitoshenko <aleksandralitoshenko@gmail.com>
2022-12-11 00:29:41 +0300

    add postres settings

--- todo/settings.py
+++ todo/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tododb',
+        'USER': 'todouser',
+        'PASSWORD': 'WW2KpN@*p)d+F6Kh',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
