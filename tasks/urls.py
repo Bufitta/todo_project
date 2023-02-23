51ab69dff96115bb1e06b6b0ad0ab04aea3767ec
alteest <alteest@gmail.com>
2023-02-23 15:04:00 +0500

    changes

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/<slug:slug>/', views.category_tasks, name='category_tasks'),
